83c39e62b68bed12fb96ba7c94e64f20586029c3
dimentor <dk2491@mail.ru>
2023-04-03 06:34:23 +0300

    Commit 3 in branch2

--- src/main/java/org/example/ClassBranch2.java
+++ src/main/java/org/example/ClassBranch2.java
@@ -3,4 +3,5 @@ package org.example;
 public class ClassBranch2 {
     int variable = 1;
     String str = "commit2InBranch2";
+    String str3 = "commit3InBranch2";
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,5 +6,6 @@ public class Main {
         System.out.println("Change in master number 1");
         System.out.println("Created branch 2");
         System.out.println("Commit 2 in branch 1");
+        System.out.println("Commit 3 in branch 2");
     }
 }
