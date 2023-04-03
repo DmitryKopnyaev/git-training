504ab1353a724ceeff08e8336df5ba86cfb73bed
dimentor <dk2491@mail.ru>
2023-04-03 06:25:23 +0300

    Commit 2 in master

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Change in master number 1");
+        System.out.println("Commit in master number 2");
     }
 }
