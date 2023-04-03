2fea82d0753b92e5ea5d27364dfd96deb5a838c6
dimentor <dk2491@mail.ru>
2023-04-03 06:13:13 +0300

    Commit 1 in branch 2

---
+++ src/main/java/org/example/ClassBranch2.java
@@ -0,0 +1,5 @@
+package org.example;
+
+public class ClassBranch2 {
+    int variable = 1;
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Change in master number 1");
+        System.out.println("Created branch 2");
     }
 }
